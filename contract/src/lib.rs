460e83b747fb8c587109494366989165dfa5d6bc
lee.aki <lee.aki.924@gmail.com>
2021-12-29 15:05:54 +0700

    Add deposit withdraw and harvest feature

--- contract/src/lib.rs
+++ contract/src/lib.rs
@@ -9,7 +9,7 @@ mod user_info;
 use pool_info::PoolInfo;
 use user_info::UserInfo;
 
-pub const ACC_REWARD_PRECISION: u32 = 10u32.pow(12);
+pub const ACC_REWARD_PRECISION: u64 = 10u64.pow(12);
 
 #[elrond_wasm::contract]
 pub trait MasterChef {
@@ -25,6 +25,7 @@ pub trait MasterChef {
             self.blockchain().is_smart_contract(&fund),
             "The fund address is not a smart contract"
         );
+
         self.reward().set(&reward);
         self.fund().set(&fund);
         self.pool_length().set(&u64::MIN);
@@ -33,26 +34,140 @@ pub trait MasterChef {
 
     /* ========== INTERNAL FUNCTIONS ========== */
 
+    /// @notice Update reward variables of the given pool.
+    /// @param pool_info
+    fn update_pool(&self, pool_info: &mut PoolInfo<Self::Api>) {
+        let block_timestamp = self.blockchain().get_block_timestamp();
+        if (block_timestamp > pool_info.last_reward_time) {
+            let lp_supply = self.blockchain().get_sc_balance(&pool_info.lp_token, 0);
+            if (lp_supply > BigUint::from(0u32)) {
+                let time: u64 = block_timestamp - &pool_info.last_reward_time;
+                let reward_amount =
+                    BigUint::from(time) * self.reward_per_second().get() * &pool_info.alloc_point
+                        / self.total_alloc_point().get();
+                pool_info.acc_reward_per_share += reward_amount * ACC_REWARD_PRECISION / lp_supply;
+            }
+            pool_info.last_reward_time = block_timestamp;
+        }
+    }
+
     /* ========== PUBLIC FUNCTIONS ========== */
 
+    //TODO: mas_update_pools, withdraw_and_harvest, harvest_all_rewards
+
+    /// @notice Deposit LP tokens to MCV2 for reward allocation.
+    /// @param pool_id The index of the pool. See `pool_info`.
+    /// @param to The receiver of `amount` deposit benefit.
     #[endpoint]
     #[payable("*")]
     fn deposit(
         &self,
         pool_id: u64,
+        to: ManagedAddress,
         #[payment_token] token: TokenIdentifier,
         #[payment] amount: BigUint,
     ) -> SCResult<()> {
-        let sender = self.blockchain().get_caller();
-        let pool_info = self.pool_info(pool_id).get();
         require!(!self.pool_info(pool_id).is_empty(), "Pool is not exist");
+        let pool_info = &mut self.pool_info(pool_id).get();
         require!(token == pool_info.lp_token, "Wrong token");
-        // TODO: UPDATE POOL
-        let mut user_info = self.user_info(&sender).get();
+        let mut user_info = self.user_info(&to).get();
+
+        // Effects
+        self.update_pool(pool_info);
         user_info.amount += amount;
         user_info.reward_debt =
-            &user_info.amount * &pool_info.acc_reward_per_share / BigUint::from(ACC_REWARD_PRECISION); // TODO: div 1e12
+            &user_info.amount * &pool_info.acc_reward_per_share / ACC_REWARD_PRECISION;
+        self.user_info(&to).set(&user_info);
+
+        // emit Deposit(&self.blockchain().get_caller(), pool_id, amount, to);
+
+        Ok(())
+    }
+
+    /// @notice Withdraw LP tokens from MasterChef.
+    /// @param pool_id The index of the pool. See `pool_info`.
+    /// @param amount LP token amount to withdraw.
+    /// @param to Receiver of the LP tokens.
+    #[endpoint]
+    fn withdraw(&self, pool_id: u64, amount: BigUint, to: ManagedAddress) -> SCResult<()> {
+        let sender = self.blockchain().get_caller();
+        require!(!self.pool_info(pool_id).is_empty(), "Pool is not exist");
+        let mut user_info = self.user_info(&sender).get();
+        require!(user_info.amount >= amount, "withdraw: not good");
+        let pool_info = &mut self.pool_info(pool_id).get();
+        self.update_pool(pool_info);
+        let accumulated_reward =
+            &user_info.amount * &pool_info.acc_reward_per_share / ACC_REWARD_PRECISION;
+        let pending_reward = &accumulated_reward - &user_info.reward_debt;
+
+        // Effects
+        user_info.amount = user_info.amount - &amount;
+        user_info.reward_debt = accumulated_reward;
         self.user_info(&sender).set(&user_info);
+
+        // Interactions
+        self.send()
+            .direct(&to, &self.reward().get(), 0, &pending_reward, &[]);
+        self.send()
+            .direct(&to, &pool_info.lp_token, 0, &amount, &[]);
+
+        // emit Withdraw(&sender, pool_id, amount, to);
+
+        Ok(())
+    }
+
+    /// @notice Withdraw without caring about rewards. EMERGENCY ONLY.
+    /// @param pool_id The index of the pool. See `poolInfo`.
+    /// @param to Receiver of the LP tokens.
+    #[endpoint]
+    fn emergency_withdraw(&self, pool_id: u64, to: ManagedAddress) -> SCResult<()> {
+        let sender = self.blockchain().get_caller();
+        require!(!self.pool_info(pool_id).is_empty(), "Pool is not exist");
+        let mut user_info = self.user_info(&sender).get();
+        let amount = &user_info.amount.clone();
+        require!(
+            user_info.amount > BigUint::from(0u32),
+            "emergency_withdraw: not good"
+        );
+        let pool_info = &mut self.pool_info(pool_id).get();
+
+        //Effects
+        user_info.amount = BigUint::from(0u32);
+        user_info.reward_debt = BigUint::from(0u32);
+        self.user_info(&sender).set(&user_info);
+
+        // Interactions
+        self.send()
+            .direct(&to, &pool_info.lp_token, 0, &amount, &[]);
+
+        // emit EmergencyWithdraw(&sender, pool_id, amount, to);
+
+        Ok(())
+    }
+
+    // /// @notice Harvest proceeds for transaction sender to `to`.
+    // /// @param pool_id The index of the pool. See `pool_info`.
+    // /// @param to Receiver of rewards.
+    #[endpoint]
+    fn harvest(&self, pool_id: u64, to: ManagedAddress) -> SCResult<()> {
+        let sender = self.blockchain().get_caller();
+        require!(!self.pool_info(pool_id).is_empty(), "Pool is not exist");
+        let pool_info = &mut self.pool_info(pool_id).get();
+        self.update_pool(pool_info);
+        let mut user_info = self.user_info(&sender).get();
+        let accumulated_reward =
+            &user_info.amount * &pool_info.acc_reward_per_share / ACC_REWARD_PRECISION;
+        let pending_reward = &accumulated_reward - &user_info.reward_debt;
+
+        // Effects
+        user_info.reward_debt = accumulated_reward;
+        self.user_info(&sender).set(&user_info);
+
+        // Interactions
+        self.send()
+            .direct(&to, &self.reward().get(), 0, &pending_reward, &[]);
+
+        // emit Harvest(&sender, pool_id, pending_reward, to);
 
         Ok(())
     }
@@ -67,28 +182,33 @@ pub trait MasterChef {
     #[endpoint]
     fn add(&self, alloc_point: BigUint, lp_token: TokenIdentifier) -> SCResult<()> {
         let pool_length = self.pool_length().get();
+        let new_pool_id = pool_length + 1;
+        let block_timestamp = self.blockchain().get_block_timestamp();
+
         for i in 1..=pool_length {
             let pool_info = self.pool_info(i).get();
             require!(pool_info.lp_token != lp_token, "Cannot add existing pool");
         }
-        let new_pool_id = pool_length + 1;
+
         self.total_alloc_point()
             .update(|total_alloc_point| *total_alloc_point += &alloc_point);
-        let last_reward_time = self.blockchain().get_block_timestamp();
+
         let new_pool = PoolInfo {
             lp_token,
             acc_reward_per_share: BigUint::zero(),
-            last_reward_time,
+            last_reward_time: block_timestamp,
             alloc_point,
         };
+
         self.pool_info(new_pool_id).set(&new_pool);
-        // emit LogPoolAddition(new_pool_id, allocPoint, lp_token);
+
+        // emit LogPoolAddition(new_pool_id, alloc_point, lp_token);
 
         Ok(())
     }
 
     /// @notice Update the given pool's reward allocation point. Can only be called by the owner.
-    /// @param pool_id The index of the pool. See `poolInfo`.
+    /// @param pool_id The index of the pool. See `pool_info`.
     /// @param alloc_point New AP of the pool.
     #[only_owner]
     #[endpoint]
@@ -125,6 +245,31 @@ pub trait MasterChef {
         // emit LogFundChanged(fund);
 
         Ok(())
+    }
+
+    /* ========== VIEW FUNCTION ========== */
+
+    /// @notice View function to see pending reward on frontend.
+    /// @param pool_id The index of the pool. See `pool_info`.
+    /// @param user Address of user.
+    /// @return pending reward for a given user.
+    #[view(pendingReward)]
+    fn pending_reward(&self, pool_id: u64, user: ManagedAddress) -> BigUint {
+        let pool_info = self.pool_info(pool_id).get();
+        let user_info = self.user_info(&user).get();
+        let mut acc_reward_per_share = pool_info.acc_reward_per_share;
+        let lp_supply = self.blockchain().get_sc_balance(&pool_info.lp_token, 0);
+        let block_timestamp = self.blockchain().get_block_timestamp();
+        if (block_timestamp > pool_info.last_reward_time && lp_supply > BigUint::from(0u32)) {
+            let time: u64 = block_timestamp - &pool_info.last_reward_time;
+            let reward_amount =
+                BigUint::from(time) * self.reward_per_second().get() * &pool_info.alloc_point
+                    / self.total_alloc_point().get();
+            acc_reward_per_share = &reward_amount * ACC_REWARD_PRECISION / lp_supply;
+        }
+        let accumulated_reward = &user_info.amount * &acc_reward_per_share / ACC_REWARD_PRECISION;
+        let pending_reward = &accumulated_reward - &user_info.reward_debt;
+        pending_reward
     }
 
     /* ========== STORAGE ========== */
